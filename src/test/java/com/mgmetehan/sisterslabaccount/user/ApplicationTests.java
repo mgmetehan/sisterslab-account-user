60961f99f5611ee5385e2e6dfdd56ea20f8369d5
mgmetehan <mgmetehan@hotmail.com>
2024-02-25 09:18:29 +0300

    init project

---
+++ src/test/java/com/mgmetehan/sisterslabaccount/user/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.sisterslabaccount.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
